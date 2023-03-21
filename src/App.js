89572fc25ebcae8c663bc09bd28a1f12263dc25d
abrmoralesm <abrmoralesm@gmail.com>
2023-03-21 10:17:47 +0100

    Exercici 2
    Farem que es mostrin totes les frases de la història.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import {Escena} from './components/escena/Escena';
+import {text} from './fakeApi/text';
 function App() {
   return (
     <div className="App">
       
        
-          <Escena />
+         {text.map ((element)=> (
+          <Escena text={element.text}/>
+       )  )}
        
     </div>
   );
